75df77a750a07ef6fe7ef07258ad6f8c8c323403
Zyabrik10 <sasamazurok@gmail.com>
2023-04-07 23:16:37 +0300

    Hw 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+import Player from '@vimeo/player';
 
+const vimeo_player = document.querySelector("#vimeo-player");
+const player = new Player(vimeo_player);
+
+const throttle = require('lodash.throttle');
+
+player.setCurrentTime(
+    Number(localStorage.getItem("videoplayer-current-time"))
+).catch(function(error) {
+    throw error;
+});
+
+player.on('timeupdate', 
+    throttle(()=>{
+        player.getCurrentTime().then(function(seconds) {
+            localStorage.setItem("videoplayer-current-time", seconds);
+        }).catch(function(error) {
+            throw error;
+        });
+    }, 1000) 
+);
+
+player.on('play', function() {
+        player.getCurrentTime().then(function(seconds) {
+            localStorage.setItem("videoplayer-current-time", seconds);
+        }).catch(function(error) {
+            throw error;
+        });
+});
+
+player.on('pause', function() {
+        player.getCurrentTime().then(function(seconds) {
+            localStorage.setItem("videoplayer-current-time", seconds);
+        }).catch(function(error) {
+            throw error;
+        });
+});
