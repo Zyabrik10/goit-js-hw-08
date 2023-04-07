82d184323a3751c94655b419e713bf62772507da
Zyabrik10 <sasamazurok@gmail.com>
2023-04-07 23:36:42 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+const form = document.querySelector('form');
+const throttle = require('lodash.throttle');
 
+form.addEventListener("input", throttle(()=>{
+    const obj = {
+        email: form.email.value,
+        message: form.message.value
+    };
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(obj));
+}, 500));
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const obj = {
+        email: form.email.value,
+        message: form.message.value
+    };
+    console.log(obj);
+    form.reset();
+    localStorage.clear();
+});
+
+window.addEventListener("load", ()=>{
+    const msg = JSON.parse(localStorage.getItem("feedback-form-state"));
+    
+
+    if (msg !== null && msg.email !== "" && msg.message !== ""){
+        form.email.value = msg.email;
+        form.message.value = msg.message;
+    }
+});
